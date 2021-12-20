3975185311b35f6e9c50f310a96ac85ec8d31b08
Jun <375105540@qq.com>
2021-12-20 16:44:55 +0800

    1.NavigationDrawer\n2.SwipeRefresh\n

---
+++ app/src/main/java/com/example/samples_compose/lh/LhApp.kt
@@ -0,0 +1,12 @@
+package com.example.samples_compose
+
+/**
+ *
+ * <b>@author：</b> shetj<br>
+ * <b>@createTime：</b> 2021/12/20<br>
+ * <b>@email：</b> 375105540@qq.com<br>
+ * <b>@describe</b>  <br>
+ */
+class LhApp {
+
+}
---
+++ composeKit/src/main/java/me/shetj/composekit/ui/weight/DrawerKit.kt
@@ -0,0 +1,11 @@
+package me.shetj.composekit.ui.weight
+
+/**
+ * 
+ * <b>@author：</b> shetj<br>
+ * <b>@createTime：</b> 2021/12/20<br>
+ * <b>@email：</b> 375105540@qq.com<br>
+ * <b>@describe</b>  <br>
+ */
+class DrawerKit {
+}
